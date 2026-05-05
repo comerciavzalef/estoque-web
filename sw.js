25ab6004449ae4587cd7e3d6dc25c46814241bbe
comerciavzalef <comercialvz.alef@gmail.com>
2026-05-05 10:51:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 //  SERVICE WORKER — Estoque Digital CRV/LAS
 // ══════════════════════════════════════════════════════════════
 
-var CACHE_NAME = 'cv-estoque-v23.1'; // Atualizado para quebrar o cache antigo
+var CACHE_NAME = 'cv-estoque-v23.2'; // Atualizado para quebrar o cache antigo
 
 var STATIC_ASSETS = [
   './',
